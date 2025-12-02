9fc79002cafa09d2fc244257f9cf12a8c9fa69f2
Nagajyothipidugu <9pidugujyothi@gmail.com>
2025-12-02 15:11:41 +0530

    message

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "project" {
+    type = string 
+    default = "roboshop"
+}
+variable "environment" {
+    type = string 
+    default = "dev"
+}
+variable "zone_id" {
+    default ="Z00583343821Q6B1A8IK6"
+}
+variable "zone_name" {
+    default ="devaws46.online"
+}
+variable "component" {
+
+}
+variable "rule_priority" {
+    
+}
